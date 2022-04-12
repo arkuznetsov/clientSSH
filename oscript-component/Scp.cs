91edffebf2fe82e6738d7e35731046c4138c554d
Artem Kuznetsov <AKuznetsov@1cbit.ru>
2022-04-12 17:12:37 +0300

    Добавлены методы работы с sftp:  - Exists (Существует) - проверка существования файла / каталога;  - CreateDirectory (СоздатьКаталог);  - DeleteDirectory (УдалитьКаталог);  - DeleteFile (УдалитьФайл); для метода UploadFile добавлен параметр-флаг перезаписи файла на сервере.

--- oscript-component/Scp.cs
+++ oscript-component/Scp.cs
@@ -26,23 +26,63 @@ namespace oscriptcomponent
             _sftpClient.Connect();
         }
 
-        
+        /// <summary>
+        /// Создать каталог
+        /// </summary>
+        /// <param name="path">Path to file or directory.</param>
+        /// <returns>
+        /// <c>true</c> if directory or file exists; otherwise <c>false</c>.
+        /// </returns>
+        [ContextMethod("Существует")]
+        public bool Exists(string path)
+        {
+
+            return _sftpClient.Exists(path);
+
+        }
+
+        /// <summary>
+        /// Создать каталог
+        /// </summary>
+        /// <param name="path">Path to new directory.</param>
+        [ContextMethod("СоздатьКаталог")]
+        public void CreateDirectory(string path)
+        {
+
+                _sftpClient.CreateDirectory(path);
+
+        }
+
+        /// <summary>
+        /// Удалить каталог
+        /// </summary>
+        /// <param name="path">Path to directory to delete.</param>
+        [ContextMethod("УдалитьКаталог")]
+        public void DeleteDirectory(string path)
+        {
+
+            _sftpClient.DeleteDirectory(path);
+
+        }
+
         /// <summary>
         /// Отправить Файл
         /// </summary>
+        /// <param name="fileName">File path.</param>
+        /// <param name="dest">Remote file path.</param>
+        /// <param name="canOwerwrite">if set to <c>true</c> then existing file will be overwritten.</param>
         /// <returns>Результат выполнения</returns>
-       [ContextMethod("ОтправитьФайл")]
-        public void UploadFile(string fileName, string dest)
+        [ContextMethod("ОтправитьФайл")]
+        public void UploadFile(string fileName, string dest, bool canOwerwrite)
         {
             
             using (var file = new FileStream(@fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
-                _sftpClient.UploadFile(file, dest);
+                _sftpClient.UploadFile(file, dest, canOwerwrite);
             }
             
         }
-        
-        
+     
         /// <summary>
         /// Получить Файл
         /// </summary>
@@ -57,7 +97,19 @@ namespace oscriptcomponent
             }
             
         }
-        
+
+        /// <summary>
+        /// Удалить файл
+        /// </summary>
+        /// <param name="path">Path to file to delete.</param>
+        [ContextMethod("УдалитьФайл")]
+        public void DeleteFile(string path)
+        {
+
+            _sftpClient.DeleteFile(path);
+
+        }
+
         /// <summary>
         /// Закрыть соединение
         /// </summary>

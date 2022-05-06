3c4214d05224c89c85c97bbc5a22369ca8608b99
Artem Kuznetsov <arkuznetsov@gmail.com>
2022-05-06 10:51:16 +0300

    test: ДОбавлнено чтение файла переменных среды в скрипт ручного запуска тестов.

--- tools/runtests.bat
+++ tools/runtests.bat
@@ -3,6 +3,12 @@
 echo %~dp0
 @chcp 65001
 
+FOR /f "usebackq tokens=*" %%a in ("%~dp0openssh\.env") DO (
+  FOR /F "tokens=1,2 delims==" %%b IN ("%%a") DO (
+    set "%%b=%%c"
+  )
+)
+
 @"C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe" curl -o "%~dp0nuget.exe" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 @%~dp0nuget.exe restore %~dp0../src
 @%~dp0nuget.exe install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory %~dp0../test

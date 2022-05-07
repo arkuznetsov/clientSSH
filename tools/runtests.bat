2a1adf09ca0b39adf3641ea5526cf1106f6012ba
Artem Kuznetsov <arkuznetsov@gmail.com>
2022-05-07 14:46:29 +0300

    test: Добавлена сборка покрытия.

--- tools/runtests.bat
+++ tools/runtests.bat
@@ -12,11 +12,12 @@ FOR /f "usebackq tokens=*" %%a in ("%~dp0openssh\.env") DO (
 @"C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe" curl -o "%~dp0nuget.exe" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 @%~dp0nuget.exe restore %~dp0../src
 @%~dp0nuget.exe install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory %~dp0../test
+@dotnet tool install --global dotnet-coverage
 @dotnet restore %~dp0../src
 @dotnet build %~dp0../src --configuration Debug
 @docker-compose --file %~dp0openssh/docker-compose.yml --env-file %~dp0openssh/.env up --build -d
 @mkdir "%~dp0../test"
-@%~dp0../test/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe %~dp0../src/NUnitTests/bin/Debug/net452/NUnitTests.dll --result=%~dp0../test/nunit-result.xml
+@dotnet-coverage collect -o %~dp0../test/coverage.xml -f xml "%~dp0../test/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe %~dp0../src/NUnitTests/bin/Debug/net452/NUnitTests.dll --result=%~dp0../test/nunit-result.xml"
 @rd /S /Q "%~dp0../test/NUnit.ConsoleRunner.3.6.1"
 @docker-compose --file %~dp0openssh/docker-compose.yml down
 @exit /b %ERRORLEVEL%

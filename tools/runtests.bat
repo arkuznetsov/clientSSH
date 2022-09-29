56491cba95251cfb1943b4ee8d900436f30bc86d
Artem Kuznetsov <arkuznetsov@gmail.com>
2022-09-29 23:29:06 +0300

    test: Скрипт ручного запуска тестирования компоненты сделан универсальным и параметризуемым.

--- tools/runtests.bat
+++ tools/runtests.bat
@@ -3,29 +3,54 @@
 echo %~dp0
 @chcp 65001
 
-set SSH_TEST_CWD=%~dp0..
-set SSH_TEST_TOOLS=%~dp0
-set SSH_TEST_SRC=%SSH_TEST_CWD%/src
-set SSH_TEST_KEY_PATH=%SSH_TEST_TOOLS%openssh/files/sftp-key
-set SSH_TEST_LIB_PATH=%SSH_TEST_CWD%/src/clientSSH/bin/Debug/net452/ClientSSH.dll
+set OSC_TEST_NAME=ClientSSH
 
-FOR /f "usebackq tokens=*" %%a in ("%~dp0openssh\.env") DO (
-  FOR /F "tokens=1,2 delims==" %%b IN ("%%a") DO (
-    set "%%b=%%c"
+set OSC_TEST_ROOT=%~dp0
+set OSC_TEST_CWD=%OSC_TEST_ROOT%..
+set OSC_TEST_SRC=%OSC_TEST_ROOT%../src
+
+set OSC_TEST_LIB=%OSC_TEST_SRC%/%OSC_TEST_NAME%/bin/Debug/net452/%OSC_TEST_NAME%.dll
+
+set OSC_TEST_TOOLS=%OSC_TEST_ROOT%
+set OSC_TEST_NUGET=%OSC_TEST_TOOLS%nuget.exe
+set OSC_TEST_NUNIT3=%OSC_TEST_TOOLS%NUnit.ConsoleRunner.3.6.1
+
+set OSC_TEST_REPORTS=%OSC_TEST_ROOT%../build/reports
+
+set SSH_TEST_KEY_PATH=%OSC_TEST_TOOLS%openssh/files/sftp-key
+
+IF EXIST "%OSC_TEST_ROOT%.env" (
+  FOR /f "usebackq tokens=*" %%a in ("%OSC_TEST_ROOT%.env") DO (
+    FOR /F "tokens=1,2 delims==" %%b IN ("%%a") DO (
+      set "%%b=%%c"
+    )
   )
 )
 
-IF NOT EXIST "%~dp0nuget.exe" (
-  @"C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe" curl -o "%~dp0nuget.exe" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+IF NOT EXIST "%OSC_TEST_NUGET%" (
+  @"C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe" curl -o "%OSC_TEST_NUGET%" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 )
-@%~dp0nuget.exe restore %~dp0../src
-@%~dp0nuget.exe install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory %~dp0../tools
+
+@%OSC_TEST_NUGET% restore %OSC_TEST_SRC%
+
+IF NOT EXIST "%OSC_TEST_NUNIT3%" (
+  @%OSC_TEST_NUGET% install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory %OSC_TEST_TOOLS%
+)
+
 @dotnet tool install --global dotnet-coverage
-@dotnet restore %~dp0../src
-@dotnet build %~dp0../src --configuration Debug
-@docker-compose --file %~dp0openssh/docker-compose.yml --env-file %~dp0openssh/.env up --build -d
-@mkdir "%~dp0../build/reports"
-@dotnet-coverage collect -o %~dp0../build/reports/coverage.xml -f xml "%~dp0../tools/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe %~dp0../src/NUnitTests/bin/Debug/net452/NUnitTests.dll --result=%~dp0../build/reports/nunit-result.xml"
-@rd /S /Q "%~dp0../tools/NUnit.ConsoleRunner.3.6.1"
-@docker-compose --file %~dp0openssh/docker-compose.yml down
+@dotnet restore %OSC_TEST_SRC%
+@dotnet build %OSC_TEST_SRC% --configuration Debug
+
+IF EXIST "%OSC_TEST_ROOT%docker-compose.yml" (
+  @docker-compose --file %OSC_TEST_ROOT%docker-compose.yml --env-file %OSC_TEST_ROOT%.env up --build -d
+)
+
+@mkdir "%OSC_TEST_REPORTS%"
+@dotnet-coverage collect -o %OSC_TEST_REPORTS%/coverage.xml -f xml "%OSC_TEST_NUNIT3%/tools/nunit3-console.exe %OSC_TEST_SRC%/NUnitTests/bin/Debug/net452/NUnitTests.dll --result=%OSC_TEST_REPORTS%/nunit-result.xml"
+rem @rd /S /Q "%OSC_TEST_NUNIT3%"
+
+IF EXIST "%OSC_TEST_ROOT%docker-compose.yml" (
+  @docker-compose --file %OSC_TEST_ROOT%docker-compose.yml down
+)
+
 @exit /b %ERRORLEVEL%
